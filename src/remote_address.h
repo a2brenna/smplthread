8f479679a751a6de162646367b65f7c85679d8fb
Anthony Brennan <a2brenna@csclub.uwaterloo.ca>
2015-03-17 16:28:57 -0400

    Define minimum viable functionality of a Remote_Address

--- src/remote_address.h
+++ src/remote_address.h
@@ -1,7 +1,16 @@
 #ifndef __REMOTE_ADDRESS_H__
 #define __REMOTE_ADDRESS_H__
 
+#include "channel.h"
+
 namespace smpl {
+
+    class Remote_Address{
+
+        public:
+            virtual Channel* connect() = 0;
+
+    };
 
 }
 
