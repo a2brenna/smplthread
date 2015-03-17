2394a1ba6e096105534781d567ebc3c8b30b572f
Anthony Brennan <a2brenna@csclub.uwaterloo.ca>
2015-03-17 16:28:22 -0400

    Define minimimum viable interface for Channel

--- src/channel.h
+++ src/channel.h
@@ -1,8 +1,20 @@
 #ifndef __CHANNEL_H__
 #define __CHANNEL_H__
 
+#include <string>
+
 namespace smpl {
 
+    class Channel{
+
+        Channel(const Channel&) = delete;
+
+        public:
+
+            virtual std::string send() = 0;
+            virtual std::string recv() = 0;
+
+    };
 
 }
 
