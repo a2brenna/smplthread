f84fde39c0c1bc384865b6877365b9c32777fce9
Anthony Brennan <a2brenna@csclub.uwaterloo.ca>
2015-03-17 16:41:07 -0400

    Fix definition error for send()

--- src/channel.h
+++ src/channel.h
@@ -13,7 +13,7 @@ namespace smpl {
 
             virtual ~Channel() = 0;
 
-            virtual std::string send() = 0;
+            virtual void send(const std::string &msg) = 0;
             virtual std::string recv() = 0;
 
     };
