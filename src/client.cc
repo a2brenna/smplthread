29de12a71eff5f7bd7f5359eb99dae7f595c8118
Anthony Brennan <a2brenna@csclub.uwaterloo.ca>
2015-03-18 16:37:18 -0400

    Simple test client and server

--- src/client.cc
+++ src/client.cc
@@ -1,3 +1,13 @@
+#include "channel.h"
+#include "unix_domain_socket.h"
+
+#include <memory>
+
 int main(){
+    std::unique_ptr<smpl::Remote_Address> server_address( new Remote_UDS("/tmp/channel_test.sock"));
+    std::unique_ptr<smpl::Channel> server( server_address->connect() );
+    const std::string message = "Hello";
+    server->send(message);
     return 0;
 }
+
--- src/server.cc
+++ src/server.cc
@@ -1,6 +1,19 @@
+#include "channel.h"
+#include "unix_domain_socket.h"
+
+#include <memory> //for std::unique_ptr
+#include <iostream> //for std::cout
+
 int main(){
 
+
+    std::unique_ptr<smpl::Local_Address> server(new Local_UDS("/tmp/channel_test.sock"));
+
     for(;;){
+
+        std::unique_ptr<smpl::Channel> client(server->listen());
+        std::string message = client->recv();
+        std::cout << message << std::endl;
 
     }
 
