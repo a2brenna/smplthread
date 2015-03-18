e9f19e9ef2bcf56364946be9692778d6c265847d
Anthony Brennan <a2brenna@csclub.uwaterloo.ca>
2015-03-18 17:04:01 -0400

    Time operations

--- src/client.cc
+++ src/client.cc
@@ -1,13 +1,23 @@
 #include "channel.h"
 #include "unix_domain_socket.h"
 
-#include <memory>
+#include <memory> //for unique_ptr
+#include <iostream>
+#include <chrono>
 
 int main(){
+    std::string message;
+    for(int i = 0; i < 10000000; i++){
+        message.append("Hello");
+    }
+
+    const auto start_time = std::chrono::high_resolution_clock::now();
     std::unique_ptr<smpl::Remote_Address> server_address( new Remote_UDS("/tmp/channel_test.sock"));
     std::unique_ptr<smpl::Channel> server( server_address->connect() );
-    const std::string message = "Hello";
     server->send(message);
+    const auto end_time = std::chrono::high_resolution_clock::now();
+
+    std::cout << "Elapsed " << (end_time - start_time).count() << std::endl;
     return 0;
 }
 
