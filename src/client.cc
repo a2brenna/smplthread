342154a45462a75384610f435225be966a8495d4
Anthony Brennan <a2brenna@csclub.uwaterloo.ca>
2015-03-19 12:54:56 -0400

    Slightly more complete tests

--- src/client.cc
+++ src/client.cc
@@ -1,24 +1,37 @@
 #include "channel.h"
 #include "unix_domain_socket.h"
+#include "network_socket.h"
+#include "hgutil/time.h"
 
 #include <memory> //for unique_ptr
 #include <iostream>
 #include <chrono>
 
-int main(){
-    std::string message;
-    message.append("Hello");
+void test_mechanism(const std::unique_ptr<smpl::Remote_Address> &server_address){
+        std::string message;
+        message.append("Hello");
+        for(int i = 0; i < 1; i++){
+            message.append("\nHello");
+        }
 
-    std::unique_ptr<smpl::Remote_Address> server_address( new Remote_UDS("/tmp/channel_test.sock"));
+        const auto start_time = std::chrono::high_resolution_clock::now();
+        std::unique_ptr<smpl::Channel> c( server_address->connect() );
+        for(int i = 0; i < 1; i++){
+            c->send(message);
+        }
+        const auto end_time = std::chrono::high_resolution_clock::now();
 
-    const auto start_time = std::chrono::high_resolution_clock::now();
-    for(int i = 0; i < 1000000; i++){
-        std::unique_ptr<smpl::Channel> server( server_address->connect() );
-        server->send(message);
-    }
-    const auto end_time = std::chrono::high_resolution_clock::now();
-
-    std::cout << "Elapsed " << (end_time - start_time).count() << std::endl;
-    return 0;
+        std::cout << "Elapsed " << (end_time - start_time).count() << std::endl;
 }
 
+int main(){
+    try{
+        std::unique_ptr<smpl::Remote_Address> server_a( new Remote_UDS("/tmp/channel_test.sock"));
+        test_mechanism(server_a);
+        std::unique_ptr<smpl::Remote_Address> server_b( new Remote_Port("127.0.0.1", 6000));
+        test_mechanism(server_b);
+    }
+    catch(...){
+        return -1;
+    }
+}
--- src/server.cc
+++ src/server.cc
@@ -1,21 +1,24 @@
 #include "channel.h"
 #include "unix_domain_socket.h"
+#include "network_socket.h"
 
 #include <memory> //for std::unique_ptr
 #include <iostream> //for std::cout
 
+void test_mechanism(const std::unique_ptr<smpl::Local_Address> &listen_point){
+    std::unique_ptr<smpl::Channel> client(listen_point->listen());
+    std::string message = client->recv();
+    std::cout << message << std::endl;
+}
+
 int main(){
 
 
-    std::unique_ptr<smpl::Local_Address> server(new Local_UDS("/tmp/channel_test.sock"));
-
-    for(;;){
-
-        std::unique_ptr<smpl::Channel> client(server->listen());
-        std::string message = client->recv();
-
+    {
+        std::unique_ptr<smpl::Local_Address> server_a(new Local_UDS("/tmp/channel_test.sock"));
+        test_mechanism(server_a);
+        std::unique_ptr<smpl::Local_Address> server_b(new Local_Port("127.0.0.1", 6000));
+        test_mechanism(server_b);
     }
-
     return 0;
-
 }
