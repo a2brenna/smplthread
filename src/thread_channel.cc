86308b8b82f5d52e80bc15ee90c059a6e3aa0891
Anthony Brennan <a2brenna@csclub.uwaterloo.ca>
2015-03-19 13:17:42 -0400

    Stub out implementation of thread_channel

--- src/thread_channel.cc
+++ src/thread_channel.cc
@@ -1,1 +1,48 @@
 #include "thread_channel.h"
+
+#include <map>
+#include <deque>
+#include <mutex>
+
+std::mutex connection_queues_lock;
+std::map<pthread_t, std::deque<pthread_t>> connection_queues;
+
+Thread_Listener::Thread_Listener(){
+
+}
+
+Thread_Listener::~Thread_Listener(){
+
+}
+
+smpl::Channel* Thread_Listener::listen(){
+    return nullptr;
+}
+
+bool Thread_Listener::check(){
+    return false;
+}
+
+Thread_ID::Thread_ID(const pthread_t &peer){
+
+}
+
+smpl::Channel* Thread_ID::connect(){
+    return nullptr;
+}
+
+Thread_Channel::Thread_Channel(){
+
+}
+
+Thread_Channel::~Thread_Channel(){
+
+}
+
+void Thread_Channel::send(const std::string &msg){
+
+}
+
+std::string Thread_Channel::recv(){
+    return "";
+}
