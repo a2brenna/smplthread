8190bae5a7269906dbe641373c774a5f8295da7e
Anthony Brennan <a2brenna@csclub.uwaterloo.ca>
2015-03-25 13:07:30 -0400

    Safe sequential test

--- src/client.cc
+++ src/client.cc
@@ -65,26 +65,31 @@ int main(){
         std::unique_ptr<smpl::Channel> client_thread_foo( server_c->listen() );
         std::cout << client_thread_foo->recv() << std::endl;
         client_thread_foo->send("World");
-        t_foo.join();
+        //t_foo.join();
 
         std::unique_ptr<smpl::Channel> client_thread_bar( server_c->listen() );
         std::cout << client_thread_bar->recv() << std::endl;
         client_thread_bar->send("World");
-        t_bar.join();
+        //t_bar.join();
 
         std::unique_ptr<smpl::Channel> client_thread_baz( server_c->listen() );
         std::cout << client_thread_baz->recv() << std::endl;
         client_thread_baz->send("World");
-        t_baz.join();
+        //t_baz.join();
 
         std::unique_ptr<smpl::Channel> client_thread_sal( server_c->listen() );
         std::cout << client_thread_sal->recv() << std::endl;
         client_thread_sal->send("World");
-        t_sal.join();
+        //t_sal.join();
 
         std::unique_ptr<smpl::Channel> client_thread_gaf( server_c->listen() );
         std::cout << client_thread_gaf->recv() << std::endl;
         client_thread_gaf->send("World");
+        //t_gaf.join();
+        t_foo.join();
+        t_bar.join();
+        t_baz.join();
+        t_sal.join();
         t_gaf.join();
     }
 
